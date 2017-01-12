d17bfbed400a29690fd5144f14ad4b0d6b9714f3
Abbondanzo <peter@abbondanzo.com>
2017-01-12 00:13:24 -0500

    Added bourbon, added CSS

--- header.php
+++ header.php
@@ -1,6 +1,9 @@
 <div id="header">
 	<div id="navigation">
 		<div class="width">
+			<div class="logo">
+				<img src="img/logo.png" alt="Logo">
+			</div>
 			<ul>
 				<li><a href="[[page:home]]">Home</a></li>
 				<li><a href="[[page:About]]">About</a></li>
--- index.php
+++ index.php
@@ -3,6 +3,15 @@
 	<?php include 'head.php'; ?>
 	<body>
 		<?php include 'header.php'; ?>
+		<div class="content">
+			<div class="valign" id="top">
+				<div class="width">
+					<h1>Northeastern VR</h1>
+					<h2>Northeastern’s first and only virtual reality and augmented reality organization</h2>
+				</div>
+			</div>
+			<!-- end above-fold content -->
+		</div>
 		<?php include 'footer.php'; ?>
 	</body>
 </html>
