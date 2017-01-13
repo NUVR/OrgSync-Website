cb57115b934367b8fb076c5e1d4577fabae65584
Abbondanzo <peter@abbondanzo.com>
2017-01-12 21:14:24 -0500

    Images, styling, and additional content

--- index.php
+++ index.php
@@ -11,6 +11,26 @@
 					<h2>Northeastern’s first and only virtual reality and augmented reality organization</h2>
 				</div>
 			</div>
+			<!-- end header -->
+			<div class="logos">
+				<div class="width">
+					<img src="img/Google.png" alt="Google">
+					<img src="img/Microsoft.png" alt="Microsoft">
+					<img src="img/Oculus.png" alt="Oculus">
+					<img src="img/Vive.png" alt="Vive">
+				</div>
+			</div>
+			<div id="about">
+				<div class="width">
+					<left>
+						<img src="img/hololens.png" alt="Microsoft HoloLens">
+					</left><right>
+						<h3>What is NUVR?</h3>
+						<p>The beautiful world of virtual and augmented reality is desperate for developers and engineers. Northeastern University Virtual Reality, or NUVR, is where students from all over campus can come together to research and develop with the newest technologies. From the Microsoft Hololens to Google Glass, NUVR’s wide array of devices offers any curious mind the chance to express themselves. Our mission is to bridge the gap that exists between the virtual developer and real world consumers. Join our organization today!</p>
+						<a href="https://orgsync.com/152063/chapter" target="_blank"><button class="btn">Join OrgSync</button></a>
+					</right>
+				</div>
+			</div>
 			<!-- end above-fold content -->
 		</div>
 		<?php include 'footer.php'; ?>
